d010226ce30128f093fb99c22ffa2af9e327a63e
ODUGUWA IMOLEAYO <onemolecastle@yahoo.com>
2026-04-22 10:03:08 +0100

    Add AWS S3 bucket and configuration resources

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "deployment_bucket_12654"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "config" {
+  bucket = aws_s3_bucket.example.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+ }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
